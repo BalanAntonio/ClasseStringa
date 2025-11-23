3be14fdc62255916188eae14a829ab6332955df0
BalanAntonio <12999441@itisrossi.vi.it>
2025-11-23 13:04:07 +0100

    finito firme

--- ClasseStringa/stringa.cpp
+++ ClasseStringa/stringa.cpp
@@ -1,16 +1,39 @@
 
 
 class classepadre {
-protected:
-	int dimensione;
-public:
-	virtual int calcolaDimensione() = 0;
+	protected:
+		int lunghezza;
+	public:
+		virtual int getLunghezza() = 0;
 };
 
 class stringa : public classepadre {
-private:
+	private:
+		char* s;
 
+	public:
+		stringa();
+		stringa(const char* s);
+		~stringa();
 
-public:
-
+		void Stampa();
+		void Reverse();
+		int  Palindroma();
+		
+		stringa operator+(stringa s2);
+		stringa operator==(stringa s2);
 };
+
+
+stringa::stringa() {
+	lunghezza = 0;
+	s = new char[1];
+}
+
+stringa::stringa(const char* s) {
+	int i = 0;
+	
+	while (s[i] != '\0') {
+		lunghezza++;
+	}
+}
